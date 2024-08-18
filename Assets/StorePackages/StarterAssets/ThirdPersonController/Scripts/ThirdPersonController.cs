eb6fa2e67f462b6b16532e1b3c6fcd8b20602c10
Aditya Singh <s.addy1317@gmail.com>
2024-08-18 14:01:30 +0530

    feat: Package - third person controller - first person controller

---
+++ Assets/StorePackages/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -0,0 +1,342 @@
+﻿using UnityEngine;
+#if ENABLE_INPUT_SYSTEM 
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    [RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM 
+    [RequireComponent(typeof(PlayerInput))]
+#endif
+    public class ThirdPersonController : MonoBehaviour
+    {
+        [Header("Player Movement")]
+        [SerializeField] private float _moveSpeed = 2.0f;
+        [SerializeField] private float _sprintSpeed = 5.335f;
+        [SerializeField] [Range(0.0f, 0.3f)] private float _rotationSmoothTime = 0.12f;
+        [SerializeField] private float _speedChangeRate = 10.0f;
+
+        [Header("Player Jump")]
+        [SerializeField] private float _jumpHeight = 1.2f;
+        [SerializeField] private float _gravity = -15.0f;
+        [SerializeField] private float _jumpTimeout = 0.50f;
+        [SerializeField] private float _fallTimeout = 0.15f;
+
+        [Header("Player Grounded")]
+        [SerializeField] private bool _grounded = true;
+        [SerializeField] private float _groundedOffset = -0.14f;
+        [SerializeField] private float _groundedRadius = 0.28f;
+        [SerializeField] private LayerMask _groundLayers;
+
+        [Header("Cinemachine")]
+        [SerializeField] private GameObject _cinemachineCameraTarget;
+        [SerializeField] private GameObject _cinemachineAimCam; 
+        [SerializeField] private GameObject _cinemachineFollowCam;
+        [SerializeField] private float _topClamp = 70.0f;
+        [SerializeField] private float _bottomClamp = -30.0f;
+        [SerializeField] private float _cameraAngleOverride = 0.0f;
+        [SerializeField] private bool _lockCameraPosition = false;
+
+        [Header("Audio")]
+        [SerializeField] private AudioClip _landingAudioClip;
+        [SerializeField] private AudioClip[] _footstepAudioClips;
+        [SerializeField] [Range(0, 1)] private float _footstepAudioVolume = 0.5f;
+
+        [Header("Bow and Arrow")]
+        [SerializeField] private GameObject _bowObject;
+        [SerializeField] private GameObject _arrowObject;
+        [SerializeField] private Transform _arrowPoint;
+
+        // cinemachine
+        private float _cinemachineTargetYaw;
+        private float _cinemachineTargetPitch;
+
+        // player attributes
+        private float _speed;
+        private float _animationBlend;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+        private float _terminalVelocity = 53.0f;
+
+        // timeout deltatime
+        private float _jumpTimeoutDelta;
+        private float _fallTimeoutDelta;
+
+        // player animation IDs
+        private int _animIDSpeed;
+        private int _animIDGrounded;
+        private int _animIDJump;
+        private int _animIDFreeFall;
+        private int _animIDMotionSpeed;
+
+#if ENABLE_INPUT_SYSTEM 
+        private PlayerInput _playerInput;
+#endif
+        private Animator _animator;
+        private CharacterController _controller;
+        private StarterAssetsInputs _input;
+        private Transform _mainCamera;
+
+        private const float _threshold = 0.01f;
+
+        private bool _hasAnimator;
+
+        private bool IsCurrentDeviceMouse
+        {
+            get
+            {
+#if ENABLE_INPUT_SYSTEM
+                return _playerInput.currentControlScheme == "KeyboardMouse";
+#else
+				return false;
+#endif
+            }
+        }
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main.transform;
+        }
+
+        private void Start()
+        {
+            _cinemachineTargetYaw = _cinemachineCameraTarget.transform.rotation.eulerAngles.y;
+
+            _hasAnimator = TryGetComponent(out _animator);
+            _controller = GetComponent<CharacterController>();
+            _input = GetComponent<StarterAssetsInputs>();
+#if ENABLE_INPUT_SYSTEM 
+            _playerInput = GetComponent<PlayerInput>();
+#else
+			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+#endif
+            PlayerAnimationIDs();
+
+            _jumpTimeoutDelta = _jumpTimeout;
+            _fallTimeoutDelta = _fallTimeout; 
+        }
+
+        private void Update()
+        {
+            //_hasAnimator = TryGetComponent(out _animator);
+            PlayerGroundedCheck();
+            
+            PlayerJumpAndGravity();
+            
+            PlayerMovement();
+
+            AimShoot();
+        }
+
+        private void LateUpdate()
+        {
+            CameraRotation();
+        }
+
+        private void PlayerAnimationIDs()
+        {
+            _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDGrounded = Animator.StringToHash("Grounded");
+            _animIDJump = Animator.StringToHash("Jump");
+            _animIDFreeFall = Animator.StringToHash("FreeFall");
+            _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+        }
+
+        private void PlayerGroundedCheck()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - _groundedOffset, transform.position.z);
+
+            _grounded = Physics.CheckSphere(spherePosition, _groundedRadius, _groundLayers, QueryTriggerInteraction.Ignore);
+
+            if (_hasAnimator)
+            {
+                _animator.SetBool(_animIDGrounded, _grounded);
+            }
+        }
+
+        private void CameraRotation()
+        {
+            if (_input.look.sqrMagnitude >= _threshold && !_lockCameraPosition)
+            {
+                float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+            }
+
+            _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, _bottomClamp, _topClamp);
+
+            _cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + _cameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+        }
+
+        private void PlayerMovement()
+        {
+            float targetSpeed = _input.sprint ? _sprintSpeed : _moveSpeed;
+
+            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+            float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+            float speedOffset = 0.1f;
+            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+            if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+            {
+                _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * _speedChangeRate);
+
+                _speed = Mathf.Round(_speed * 1000f) / 1000f;
+            }
+            else
+            {
+                _speed = targetSpeed;
+            }
+
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * _speedChangeRate);
+
+            if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+            Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+            if (_input.move != Vector2.zero)
+            {
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, _rotationSmoothTime);
+
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+            _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+            if (_hasAnimator)
+            {
+                _animator.SetFloat(_animIDSpeed, _animationBlend);
+                _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+            }
+        }
+
+        private void PlayerJumpAndGravity()
+        {
+            if (_grounded)
+            {
+                _fallTimeoutDelta = _fallTimeout;
+
+                if (_hasAnimator)
+                {
+                    _animator.SetBool(_animIDJump, false);
+                    _animator.SetBool(_animIDFreeFall, false);
+                }
+
+                if (_verticalVelocity < 0.0f)
+                {
+                    _verticalVelocity = -2f;
+                }
+
+                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+                {
+                    _verticalVelocity = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+
+                    if (_hasAnimator)
+                    {
+                        _animator.SetBool(_animIDJump, true);
+                    }
+                }
+
+                if (_jumpTimeoutDelta >= 0.0f)
+                {
+                    _jumpTimeoutDelta -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                _jumpTimeoutDelta = _jumpTimeout;
+
+                if (_fallTimeoutDelta >= 0.0f)
+                {
+                    _fallTimeoutDelta -= Time.deltaTime;
+                }
+                else
+                {
+                    if (_hasAnimator)
+                    {
+                        _animator.SetBool(_animIDFreeFall, true);
+                    }
+                }
+
+                _input.jump = false;
+            }
+
+            if (_verticalVelocity < _terminalVelocity)
+            {
+                _verticalVelocity += _gravity * Time.deltaTime;
+            }
+        }
+
+        private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+        {
+            if (lfAngle < -360f) lfAngle += 360f;
+            if (lfAngle > 360f) lfAngle -= 360f;
+            return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+            Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+            if (_grounded) Gizmos.color = transparentGreen;
+            else Gizmos.color = transparentRed;
+
+            Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - _groundedOffset, transform.position.z), _groundedRadius);
+        }
+
+        private void OnFootstep(AnimationEvent animationEvent)
+        {
+            if (animationEvent.animatorClipInfo.weight > 0.5f)
+            {
+                if (_footstepAudioClips.Length > 0)
+                {
+                    var index = Random.Range(0, _footstepAudioClips.Length);
+                    AudioSource.PlayClipAtPoint(_footstepAudioClips[index], transform.TransformPoint(_controller.center), _footstepAudioVolume);
+                }
+            }
+        }
+
+        private void OnLand(AnimationEvent animationEvent)
+        {
+            if (animationEvent.animatorClipInfo.weight > 0.5f)
+            {
+                AudioSource.PlayClipAtPoint(_landingAudioClip, transform.TransformPoint(_controller.center), _footstepAudioVolume);
+            }
+        }
+
+        private void AimShoot()
+        {
+            if(_input.isAiming && _grounded && !_input.sprint)
+            {
+                _animator.SetBool("Aiming", _input.isAiming);
+                _animator.SetBool("Shooting", _input.isShooting);
+                _cinemachineFollowCam.SetActive(false);
+                _cinemachineAimCam.SetActive(true);
+            }
+            else
+            {
+                _animator.SetBool("Aiming", false);
+                _animator.SetBool("Shooting", false);
+                _cinemachineFollowCam.SetActive(true);
+                _cinemachineAimCam.SetActive(false);
+            }
+        }
+
+        public void Shoot()
+        {
+            Debug.Log("Throw Arrow");
+            GameObject arrow = Instantiate(_arrowObject, _arrowPoint.position,this.transform.rotation);
+            arrow.GetComponent<Rigidbody>().AddForce(transform.forward * 25f,ForceMode.Impulse);
+        }
+    }
+}
